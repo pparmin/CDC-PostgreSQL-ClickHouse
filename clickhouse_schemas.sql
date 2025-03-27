1162c39722c1a56db4210b7dc3bda8bff595ce9c
Philipp Armingeon <49003037+pparmin@users.noreply.github.com>
2025-03-27 15:57:55 -0500

    Added clickhouse_schemas.sql

---
+++ clickhouse_schemas.sql
@@ -0,0 +1,58 @@
+CREATE TABLE default.bookings 
+(
+	`booking_id` String,
+	`status` String,
+	`is_deleted` UInt8,
+	`is_canceled` Bool,
+	`created_at` DateTime('UTC'),
+	`modified_at` DateTime('UTC'),
+	`version` UInt64, 
+)
+ENGINE = ReplacingMergeTree(version, is_deleted)
+PRIMARY KEY booking_id
+ORDER BY booking_id
+
+CREATE TABLE bookings_changes
+(
+	`before.id` UInt64,
+	`before.booking_id` String,
+	`before.status` String,
+	`before.is_deleted` UInt8,
+	`before.is_canceled` Bool,
+	`before.created_at` DateTime('UTC'),
+	`before.modified_at` DateTime('UTC'),
+	`after.id` UInt64,
+	`after.booking_id` String,
+	`after.status` String,
+	`after.is_deleted` UInt8,
+	`after.is_canceled` Bool,
+	`after.created_at` DateTime('UTC'),
+	`after.modified_at` DateTime('UTC'),
+	`op` LowCardinality(String), 
+	`ts_ms` UInt64, 
+	`source.sequence` String, 
+	`source.lsn` UInt64
+)
+ENGINE = MergeTree 
+ORDER BY tuple()
+
+	CREATE MATERIALIZED VIEW bookings_mv TO bookings
+	(
+	`booking_id` String,
+	`status` String,
+	`is_deleted` UInt8,
+	`is_canceled` Bool,
+	`created_at` DateTime,
+	`modified_at` DateTime,
+	`version` UInt64
+	) AS 
+	SELECT 
+		if(op = 'd', before.booking_id, after.booking_id) AS booking_id, 
+		if(op = 'd', before.status, after.status) AS status, 
+		if(op = 'd', 1, 0) AS is_deleted, 
+		if(op = 'd', before.is_canceled, after.is_canceled) AS is_canceled, 
+		if(op = 'd', before.created_at, after.created_at) AS created_at,
+		if(op = 'd', before.modified_at, after.modified_at) AS modified_at,
+		if(op = 'd', source.lsn, source.lsn) AS version
+	FROM default.bookings_changes
+	WHERE (op = 'c') OR (op = 'r') OR (op = 'u') OR (op = 'd')
